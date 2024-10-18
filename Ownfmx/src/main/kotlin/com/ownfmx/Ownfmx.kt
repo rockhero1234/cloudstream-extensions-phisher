b9bf4a3de20559fd1286cd99665aebc79131e28c
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-10-18 15:35:21 +0530

    Update Ownfmx.kt

--- Ownfmx/src/main/kotlin/com/ownfmx/Ownfmx.kt
+++ Ownfmx/src/main/kotlin/com/ownfmx/Ownfmx.kt
@@ -16,6 +16,9 @@ class Ownfmx : MainAPI() { // all providers must be an instance of MainAPI
     override val hasMainPage = true
     override var lang = "hi"
     override val hasDownloadSupport = true
+    val headerss = mapOf(
+    "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0"
+    )
     override val supportedTypes = setOf(
         TvType.Movie,
     )
@@ -43,7 +46,7 @@ class Ownfmx : MainAPI() { // all providers must be an instance of MainAPI
     ): HomePageResponse {
         
         val url = "$mainUrl/movies?page=1/"
-        val document = app.get(url).document
+        val document = app.get(url,headers=headerss).document
         Log.d("data",document.toString())
         Log.d("movies",document.select(".movie-card").toString())
         val home = document.select(".movie-card").mapNotNull {
