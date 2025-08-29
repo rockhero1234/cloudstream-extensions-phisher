1b73585eb4bce0bda61a7c6ae4e640ed5db9e7aa
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2025-08-30 00:10:57 +0530

    Update IndianTVPlugin.kt

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -88,11 +88,10 @@ class IndianTVPlugin : MainAPI() {
 
     override suspend fun load(url: String): LoadResponse {
         val data = parseJson<LoadData>(url)
-        return newLiveStreamLoadResponse(data.title,data.url)
+        return newLiveStreamLoadResponse(data.title,url,url)
         {
             this.posterUrl=data.poster
             this.plot=data.nation
-            this.dataUrl=url
         }
     }
     data class LoadData(
